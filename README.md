aae3a65c85f715a378737003bf6a0d6c8ef4b0ff
LeandroGastonLuna <lea21luna@gmail.com>
2024-04-30 17:59:42 -0300

    idea inicial

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PreEntrega1-LunaLeandro
-Js store simulator
+Js FastFood store simulator
+
+Este programa simula ser un paso a paso de una compra de comida rápida. Donde el usuario puede elegir las comidas, bebida y si desea algun extra. Y luego se muestra el precio total a pagar. 
---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+alert("¡Bienvenidx a FastFeed!\n\ \n\Vamos a tomar su orden")
+
+let comida = prompt("Seleccione Comida: \n\
+        1. Hamburguesa Completa -> $4500\n\
+        2. Hamburguesa con queso -> $3500\n\
+        3. Churrasquito caramelizado -> $5000\n\
+        4. Opcion Vegano -> $5000\n\
+        5. Omitir\n\ \n\
+Cada opcion incluye guarnición de papas")
+
+let bebida = prompt("Seleccione Bebida: \n\
+        1. Gaseosa -> $1000\n\
+        2. Agua con gas -> $800\n\
+        3. Jugo Exprimido -> $1500\n\
+        4. Omitir")
+
+
+console.log(comida)
+
+console.log(bebida)
