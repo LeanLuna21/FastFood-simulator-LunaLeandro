8452dbd6819c2ee02f2f21caa718d908bab443c0
LeandroGastonLuna <lea21luna@gmail.com>
2024-05-01 11:56:21 -0300

    logica implementada y README actualizado+

--- js/main.js
+++ js/main.js
@@ -1,21 +1,95 @@
 
-alert("¡Bienvenidx a FastFeed!\n\ \n\Vamos a tomar su orden")
-
-let comida = prompt("Seleccione Comida: \n\
-        1. Hamburguesa Completa -> $4500\n\
-        2. Hamburguesa con queso -> $3500\n\
-        3. Churrasquito caramelizado -> $5000\n\
-        4. Opcion Vegano -> $5000\n\
-        5. Omitir\n\ \n\
-Cada opcion incluye guarnición de papas")
-
-let bebida = prompt("Seleccione Bebida: \n\
-        1. Gaseosa -> $1000\n\
-        2. Agua con gas -> $800\n\
-        3. Jugo Exprimido -> $1500\n\
-        4. Omitir")
+alert("¡Bienvenidx a FastFeed!\n\ \n\ Vamos a tomar su orden.\n\ Siga los pasos hasta completar el pedido.")
 
 
-console.log(comida)
+let total_a_pagar = 0
 
-console.log(bebida)
+function tomar_orden() {
+
+    let comida
+    do {
+        comida = parseInt(prompt("Seleccione Comida: \n\
+        1. Hamburguesa Completa -> $3000\n\
+        2. Churrasquito caramelizado -> $4500\n\
+        3. Opcion vegana -> $5000\n\
+        4. Omitir / Continuar\n\ \n\
+        Cada opcion incluye guarnición de papas"))
+
+        switch (comida) {
+            case 1:
+                console.log("Agregado: hamburguesa completa")
+                total_a_pagar += 3000
+                break
+            case 2:
+                console.log("Agregado: churrasquito caramelizado")
+                total_a_pagar += 4500
+                break
+            case 3:
+                console.log("Agregado: menu vegano")
+                total_a_pagar += 5000
+                break
+            case 4:
+                break
+            default:
+                console.log("La opcion elegida no es valida.")
+        }
+
+    } while (comida < 1 || comida > 4 || isNaN(comida))
+
+    
+    let bebida
+    do {
+        bebida = parseInt(prompt("Seleccione Bebida: \n\
+    1. Gaseosa -> $1000\n\
+    2. Agua sin gas -> $800\n\
+    3. Jugo Exprimido -> $1500\n\
+    4. Omitir"))
+
+        switch (bebida) {
+            case 1:
+                console.log("Agregado: gaseosa")
+                total_a_pagar += 1000
+                break
+            case 2:
+                console.log("Agregado: agua sin gas")
+                total_a_pagar += 800
+                break
+            case 3:
+                console.log("Agregado: jugo exprimido")
+                total_a_pagar += 1500
+                break
+            case 4:
+                break
+            default:
+                console.log("la opcion elegida no es valida")
+        }
+    } while (bebida < 1 || bebida > 4 || isNaN(bebida))
+
+    if (comida != 4 || bebida != 4){
+        let extra = prompt("¿Desea agrandar el combo por $500?")
+            if (extra === null){
+                extra = "no"
+            }
+            if (extra == "" || extra.toLowerCase() === "si") {
+                console.log("Combo agrandado")
+                total_a_pagar += 500
+            } else {
+                console.log("Tamaño regular")
+            }
+        }
+}
+
+let decision
+do {
+    tomar_orden()
+    decision = prompt("¿Desea realizar otra orden (si/no)?")
+    if (decision === null) {
+        decision = "no"
+    }
+} while (decision.toLowerCase() != "no")
+
+
+console.log("Total:")
+console.log(total_a_pagar)
+
+alert("El total de su pedido es de: $" + total_a_pagar.toString())
