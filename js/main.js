8ba624f4d4abb6196cc4dea52f992406b5347b11
LeandroGastonLuna <lea21luna@gmail.com>
2024-05-17 17:15:46 -0300

    comentarios agregados

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-
+// bot de toma de pedidos
 alert("¡Bienvenidx a FastFeed!\n\ \n\ Vamos a tomar su orden.\n\ Siga los pasos hasta completar el pedido.")
 
-
+// array donde se almacenaran los pedidos
 let pedido = []
 
+// funcion que pide al usuario seleccionar su menu
 function tomarOrden() {
+
+    // variable para almacenar el precio total por orden
     let totalPedido = 0
 
     let menuComida
+    // pedimos al usuario que ingrese opcion siempre y cuando sea una de las disponibles
     do {
         menuComida = parseInt(prompt("Seleccione Comida: \n\
         1. Hamburguesa Completa -> $3000\n\
@@ -17,6 +21,7 @@ function tomarOrden() {
         Cada opcion incluye guarnición de papas"))
 
         switch (menuComida) {
+            // mostramos la opcion por consola y sumamos el precio al total de este pedido
             case 1:
                 console.log("Agregado: hamburguesa completa")
                 totalPedido += 3000
@@ -34,10 +39,10 @@ function tomarOrden() {
             default:
                 console.log("La opcion elegida no es valida.")
         }
-
     } while (menuComida < 1 || menuComida > 4 || isNaN(menuComida))
 
     let menuBebida
+    // repetimos para la bebida
     do {
         menuBebida = parseInt(prompt("Seleccione Bebida: \n\
     1. Gaseosa -> $1000\n\
@@ -65,9 +70,11 @@ function tomarOrden() {
         }
     } while (menuBebida < 1 || menuBebida > 4 || isNaN(menuBebida))
 
-
+    // si el usuario realizo algun pedido, ya sea comida o gaseosa
     if (menuComida != 4 || menuBebida != 4) {
+        // preguntamos si desea agrandar su orden
         let extra = prompt("¿Desea agrandar el combo por $500?")
+        // mostramos el resultado por consola y lo sumamos al total del pedido
         if (extra === null) {
             extra = "no"
         }
@@ -78,6 +85,8 @@ function tomarOrden() {
             console.log("Tamaño regular")
         }
     }
+
+    // por ultimo, agregamos las opciones elegidas (comida, bebida y total por pedido) al array de pedidos 
     let pedidoAgregar = {
         comida: menuComida,
         bebida: menuBebida,
@@ -86,25 +95,29 @@ function tomarOrden() {
     pedido.push(pedidoAgregar)
 }
 
+// funcion principal (corre al iniciarse) - 
 let decision
 do {
+    // llama a la funcion para tomar un pedido
     tomarOrden()
+
+    // y pregunta si desea realizar algun otro pedido
     decision = prompt("¿Desea realizar otra orden (si/no)?")
+
+    // si se apreta el boton "cancelar" entonces la desicion es "no" > esto para que al llamar a .toLowerCase no de error
     if (decision === null) {
         decision = "no"
     }
 } while (decision.toLowerCase() != "no")
 
-
+// funcion que calcula el monto total de las ordenes realizadas para mostrar al usuario el importe a pagar.
 function calcularTotal() {
-        let totalAPagar = pedido.reduce((valorTotal, pedido) => valorTotal + pedido.importe, 0)
-        return totalAPagar
-    }
+    let totalAPagar = pedido.reduce((valorTotal, pedido) => valorTotal + pedido.importe, 0)
+    return totalAPagar
+}
 
 let importeTotal = calcularTotal()
 
-console.log("Total: ")
-
-console.log(importeTotal)
+console.log("Total a abonar: ", importeTotal)
 
 alert("El total de su pedido es de: $" + importeTotal.toString())
