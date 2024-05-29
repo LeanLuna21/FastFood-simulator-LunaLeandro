91d8baba834b6daac989fb4028b4788b8cd26b68
LeandroGastonLuna <lea21luna@gmail.com>
2024-05-29 00:41:43 -0300

    dark mode function update

--- js/menu.js
+++ js/menu.js
@@ -267,23 +267,54 @@ function eliminarDelCarrito(menuID) {
     mostrarCarrito()
 }
 
+/////////// dark mode ////////////////
 
-
-// dark mode toggler
+// elementos que van a cambiar con el toggler
 let btnColorMode = document.querySelector("#color-mode");
 let btnColorIcon = document.querySelector("#color-mode-icon");
 let nav = document.querySelector(".navbar")
 
-btnColorMode.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-    if (document.body.classList.contains("dark-mode")) {
-        btnColorIcon.classList.replace("fa-lightbulb","fa-moon")
-        nav.classList.replace("navbar-light","navbar-dark")
-        nav.classList.replace("bg-light","bg-dark")
-    } else {
-        btnColorIcon.classList.replace("fa-moon","fa-lightbulb")
-        nav.classList.replace("navbar-dark","navbar-light")
-        nav.classList.replace("bg-dark","bg-light")
-        
+// funcion que evalua el tema al activar el boton toggler
+function setColorMode(){
+    let colorMode = localStorage.getItem('theme')
+
+    if (colorMode == 'light'){
+        setDarkmode()
+    } else if (colorMode == 'dark'){
+        setLightmode()
     }
+}
+
+// setea el local storage en "dark" y hace los cambios del modo al html 
+function setDarkmode(){
+    localStorage.setItem('theme','dark')
+    document.body.classList.add("dark-mode")
+    btnColorIcon.classList.replace("fa-lightbulb","fa-moon")
+    nav.classList.replace("navbar-light","navbar-dark")
+    nav.classList.replace("bg-light","bg-dark")
+
+}
+
+// setea el local storage en "light" y hace los cambios del modo al html 
+function setLightmode(){
+    document.body.classList.remove("dark-mode")
+    localStorage.setItem('theme','light')
+    btnColorIcon.classList.replace("fa-moon","fa-lightbulb")
+    nav.classList.replace("navbar-dark","navbar-light")
+    nav.classList.replace("bg-dark","bg-light")
+
+}
+
+// dark mode toggler event listener
+btnColorMode.addEventListener("click",() => {
+    setColorMode() 
 })
+
+// evalua si hay un tema guardado en el localstorage para que la pag recargue en el tema que estaba
+let colorMode = localStorage.getItem('theme')
+if (!colorMode) {
+    setLightmode()
+} else if (colorMode == "dark"){
+    setDarkmode()
+}
+
